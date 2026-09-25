9d3c2948e776ed72be4b61eaa9ef79d624c11f2c
Datharris <samuel.harris@eleve.ensai.fr>
2026-09-25 14:22:11 +0000

    ajout table test

---
+++ data/pop_db_test.sql
@@ -0,0 +1,3 @@
+INSERT INTO movies(id_movie, title, runtime, genre, plot) VALUES
+(1, 'The Odyssee', 298, 'Adventure', 'Il est parti loin et essai de revenir.'),
+(2, 'Les affranchis', 400, 'Mafia', 'Il voulait pas partir et a été obligé.');
